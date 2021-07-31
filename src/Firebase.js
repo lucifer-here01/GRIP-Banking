aca9d5b217c5174e978f44959b4e7d3da6afe486
Rishi Kumar <rishikumar147.rk@gmail.com>
2021-07-31 14:37:51 +0530

    ss

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,15 @@
 import  firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyADsRtQjqRoU7eObQ56coohlpWq-C8fPrs",
-  authDomain: "grip-banking.firebaseapp.com",
-  databaseURL: "https://grip-banking-default-rtdb.firebaseio.com",
-  projectId: "grip-banking",
-  storageBucket: "grip-banking.appspot.com",
-  messagingSenderId: "529269905953",
-  appId: "1:529269905953:web:704d29ff61b9cb58cce160"
+  
+  apiKey: "AIzaSyDu9sVNgLMIoi_6bYUPc8eco5bKYKLCxYo",
+  authDomain: "grip-banking-e2cfb.firebaseapp.com",
+  projectId: "grip-banking-e2cfb",
+  storageBucket: "grip-banking-e2cfb.appspot.com",
+  messagingSenderId: "346135955762",
+  appId: "1:346135955762:web:39f1f1da43a06c807e6f2b",
+  measurementId: "G-P7C1WKC9RC"
+
 };
 
 // Initialize Firebase
